c03b2de33feb25df6618dc9d2f22292609f82605
Josh Samuelson <js@puppetlabs.com>
2016-05-26 13:38:54 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,20 +11,6 @@
 
 ## Active Configurations ##
 
-# PRIMARY FILEBUCKET
-# This configures puppet agent and puppet inspect to back up file contents when
-# they run. The Puppet Enterprise console needs this to display file contents
-# and differences.
-
-# Define filebucket 'main':
-filebucket { 'main':
-  server => 'master.puppetlabs.vm',
-  path   => false,
-}
-
-# Make filebucket 'main' the default backup location for all File resources:
-File { backup => 'main' }
-
 # DEFAULT NODE
 # Node definitions in this file are merged with node data from the console. See
 # http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
