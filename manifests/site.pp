489e3af551a29b0bce01c9127a2ea4528fcd55a7
Ben Ford <ben.ford@puppetlabs.com>
2016-06-09 13:57:10 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,5 +25,9 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  
+  # example code for the classroom
+  include examples::puppetize
+  
   notify { "This is the default message from the production environment": }
 }
