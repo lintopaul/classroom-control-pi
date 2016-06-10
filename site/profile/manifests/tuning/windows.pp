d7066fd011410cea15a8a74f1c8ab964897f295f
Ben Ford <ben.ford@puppetlabs.com>
2016-06-10 12:37:50 -0700

    Create windows.pp

---
+++ site/profile/manifests/tuning/windows.pp
@@ -0,0 +1,10 @@
+class profile::tuning::windows {
+    registry::value { 'KeepAliveInterval':
+      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
+      value  => 'KeepAliveInterval',
+      data   => '1',
+    }
+
+  # Add more registry resources as directed
+  
+}
